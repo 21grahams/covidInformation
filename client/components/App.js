ef232ba644baf2a34778de02d0ca47bdae21a883
21grahams <graham.kirsh@gmail.com>
2021-09-30 11:18:27 -0600

    npm install react bootstrap, added modal for login

--- client/components/App.js
+++ client/components/App.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Map from './Map';
-import styles from '../../dist/styles.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Map from "./Map";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignIn from "./SignIn";
+import Button from 'react-bootstrap/Button';
 
 const App = () => {
-
   const [data, setData] = useState([]);
+  const [modalShow, setModalShow] = React.useState(false);
 
   const getAllData = () => {
-    axios.get('/getStats')
-      .then(res => setData(res.data))
-      .catch(err => console.log('Error with Get Request: ', err));
+    axios
+      .get("/getStats")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log("Error with Get Request: ", err));
   };
 
   useEffect(() => {
@@ -19,9 +22,19 @@ const App = () => {
 
   return (
     <div>
+      <>
+        <Button className='signUp' variant="primary" onClick={() => setModalShow(true)}>
+          Sign Up
+        </Button>
+
+        <SignIn
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+        />
+      </>
       <Map data={data.Countries} setData={setData} global={data.Global} />
     </div>
-  )
-}
+  );
+};
 
 export default App;

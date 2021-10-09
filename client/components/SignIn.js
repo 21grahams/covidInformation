b901aeb78b755557377ecf19bedd55ec263475ac
21grahams <graham.kirsh@ext.myfitnesspal.com>
2021-10-09 16:56:34 -0600

    added validation for to modal form

--- client/components/SignIn.js
+++ client/components/SignIn.js
@@ -7,18 +7,45 @@ import Form from "react-bootstrap/Form";
 function SignIn(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
 
+  const validate = () => {
+    let nameError = '';
+    let emailError = '';
+    
+    if (!name) {
+      nameError = 'name cannot be blank'
+    }
+    if (!email.includes('@') || !email.includes('.')) {
+      emailError = 'invalid email'
+    }
+    if (emailError || nameError) {
+      setNameError(nameError);
+      setEmailError(emailError);
+      return false;
+    }
+    return true;
+  }
+  
   const handleSubmit = (e) => {
+    let nameError = '';
+    let emailError = '';
     e.preventDefault();
+  
     let login = {
       name: name,
       email: email
     };
-    axios.post('/userInfo', login)
+
+    const isValid = validate();
+    if (isValid) {
+      axios.post('/userInfo', login)
       .then(res => alert('Thank you for joining!'))
       .catch(err => console.log('Error With Registration', err));
-    props.onHide();
-  }
+      props.onHide();
+    }
+  };
 
   return (
     <Modal
@@ -36,10 +63,16 @@ function SignIn(props) {
             <Form.Label>Name</Form.Label>
             <Form.Control name='name' value={name} onChange={(e) => setName(e.target.value)} placeholder="Jane Smith" />
           </Form.Group>
+         
+          <p style={{color: 'red'}}>{nameError}</p>
+          
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Email Address</Form.Label>
             <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="name@example.com" />
           </Form.Group>
+          
+          <p style={{color: 'red'}}>{emailError}</p>
+          
         </Form>
       </Modal.Body>
       <Modal.Footer>
